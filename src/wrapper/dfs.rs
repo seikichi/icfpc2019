c358b2ff4fde64ebcb79dbabe61853e6162fdfff
Seiichi KONDO <seikichi@kmc.gr.jp>
2019-06-21 22:42:22 +0900

    Add enums for dfs wrapper

--- src/wrapper/dfs.rs
+++ src/wrapper/dfs.rs
@@ -1,11 +1,28 @@
 use crate::solution::Solution;
-use crate::task::Task;
+use crate::task::{BoosterCode, Task};
 use crate::wrapper::Wrapper;
 
 pub struct DfsWrapper {}
 
+enum Square {
+    Surface,
+    WrappedSurface,
+    Obstacle,
+    Booster { code: BoosterCode },
+}
+
 impl Wrapper for DfsWrapper {
     fn wrap(&mut self, task: &mut Task) -> Solution {
-        Solution(vec![])
+        let mut solution = vec![];
+        while let Some(Solution(s)) = self.dfs(task) {
+            solution.extend(s);
+        }
+        Solution(solution)
+    }
+}
+
+impl DfsWrapper {
+    fn dfs(&mut self, task: &mut Task) -> Option<Solution> {
+        None
     }
 }
