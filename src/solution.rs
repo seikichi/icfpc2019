6a60cb3445306a74943cb91e35448cee7a64a2d2
MORI Shingo <cos@kmc.gr.jp>
2019-06-21 22:44:41 +0900

    add Solution::to_string

--- src/solution.rs
+++ src/solution.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord)]
 pub enum Action {
     MoveUp,
     MoveDown,
@@ -11,4 +12,28 @@ pub enum Action {
     AttachDrill,
 }
 
+#[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord)]
 pub struct Solution(pub Vec<Action>);
+
+impl Solution {
+    fn to_string(&self) -> String {
+        let Solution(actions) = self;
+        let mut ret = "".to_string();
+        for action in actions.iter() {
+            let s = match action {
+                Action::MoveUp => "W".to_string(),
+                Action::MoveDown => "S".to_string(),
+                Action::MoveLeft => "A".to_string(),
+                Action::MoveRight => "D".to_string(),
+                Action::DoNothing => "Z".to_string(),
+                Action::TurnCW => "E".to_string(),
+                Action::TurnCCW => "Q".to_string(),
+                Action::AttachManipulator { dx, dy } => format!("B({},{}))", dx, dy),
+                Action::AttachFastWheels => "F".to_string(),
+                Action::AttachDrill => "L".to_string(),
+            };
+            ret.push_str(&s);
+        }
+        return ret;
+    }
+}
