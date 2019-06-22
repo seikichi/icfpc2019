5ca2a202689f2f3a9db3ce072af36d3d4956847d
Seiichi KONDO <seikichi@kmc.gr.jp>
2019-06-22 14:12:12 +0000

    Update tools/mining.js

--- tools/mining.js
+++ tools/mining.js
@@ -1,5 +1,6 @@
 const utils = require('./utils');
 
+const exec = require('child_process').exec;
 const path = require('path');
 const fs = require('fs');
 const fetch = require('node-fetch');
@@ -8,35 +9,52 @@ const publicId = '99';
 
 const wrapper = '/home/ec2-user/src/icfpc2019/target/release/main_cloning';
 
+async function getblockinfo() {
+  const response = await fetch('http://127.0.0.1:8332/', {
+    method: 'POST',
+        headers: {
+          'Content-Type': 'text/plain',
+        },
+    body: JSON.stringify({
+      id: 'curl',
+      jsonrpc: '2.0',
+      method: 'getblockinfo',
+    }),
+  });
+  const json = await response.json();
+  if (json.error || (json.result && json.result.errors)) {
+    throw new Error(`getblockinfo request failed, ${JSON.stringify(json)}`);
+  }
+  return json;
+}
+
+async function submit(params) {
+  const response = await fetch('http://127.0.0.1:8332/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+    body: JSON.stringify({
+      id: 'curl',
+      jsonrpc: '2.0',
+      method: 'submit',
+      params,
+    }),
+  });
+  const json = await response.json();
+  if (json.error || (json.result && json.result.errors)) {
+    throw new Error(`submit request failed, ${JSON.stringify(json)}`);
+  }
+  return json;
+}
+
 (async () => {
   while (true) {
     console.log("Sleep 10 seconds ...");
     await new Promise(resolve => setTimeout(resolve, 10 * 1000));
 
     try {
-      const response = await fetch('http://127.0.0.1:8332/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'text/plain',
-        },
-        body: JSON.stringify({
-          id: 'curl',
-          jsonrpc: '2.0',
-          method: 'getblockinfo',
-        }),
-      });
-      const json = await response.json();
-      if (json.error || (json.result && json.result.errors)) {
-        console.log(`getblockinfo request failed, ${JSON.stringify(json)}`);
-        continue;
-      }
-
-      const {
-        block,
-        puzzle,
-        task,
-        excluded,
-      } = json.result;
+      const { block, puzzle, task, excluded } = (await getblockinfo()).result;
       console.log(`block = ${block}`);
 
       if (excluded.indexOf(publicId) !== -1) {
@@ -52,12 +70,23 @@ const wrapper = '/home/ec2-user/src/icfpc2019/target/release/main_cloning';
       fs.writeFileSync(inPuzzlePath, puzzle);
       fs.writeFileSync(inTaskPath, task);
 
+      await new Promise((resolve, reject) => {
+        exec(`${wrapper} < ${inTaskPath} > ${outSolutionPath}`, error => {
+          if (error) {
+            reject(error);
+            return;
+          }
+          resolve();
+        });
+      });
+
       // T.B.D.
-      // const solutionResult = await utils.checkSolution(inTaskPath, outSolutionPath);
-      // if (!solutionResult.success) {
-      //   console.log(`Solution Chcker Failed: ${JSON.stringify(solutionResult)}`);
-      //   continue;
-      // }
+
+      const solutionResult = await utils.checkSolution(inTaskPath, outSolutionPath);
+      if (!solutionResult.success) {
+        console.log(`Solution Chcker Failed: ${JSON.stringify(solutionResult)}`);
+        continue;
+      }
 
       // const puzzleResult = await utils.checkPuzzle(inPuzzlePath, outTaskPath);
       // if (!puzzleResult.success) {
@@ -65,30 +94,12 @@ const wrapper = '/home/ec2-user/src/icfpc2019/target/release/main_cloning';
       //   continue;
       // }
 
-      const params = [
-        block,
-        path.resolve(outSolutionPath),
-        path.resolve(outTaskPath),
-      ];
-
-      const submitResponse = await fetch('http://127.0.0.1:8332/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'text/plain',
-        },
-        body: JSON.stringify({
-          id: 'curl',
-          jsonrpc: '2.0',
-          method: 'submit',
-          params,
-        }),
-      });
-      const submitJson = await submitResponse.json();
-      if (submitJson.error || (submitJson.result && submitJson.result.errors)) {
-        console.log(`submit request failed, ${JSON.stringify(submitJson)}`);
-        continue;
-      }
-      console.log(`Done: ${JSON.stringify(submitJson)}`);
+      // const result = submit([
+      //   block,
+      //   path.resolve(outSolutionPath),
+      //   path.resolve(outTaskPath),
+      // ]);
+      // console.log(`Done: ${JSON.stringify(result)}`);
     } catch (e) {
       console.log(e);
     }
