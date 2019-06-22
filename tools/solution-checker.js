91d9343a509bdd6d4bcd01ba17d44df8d1b5df59
Seiichi KONDO <seikichi@kmc.gr.jp>
2019-06-22 12:51:40 +0000

    Speed up update.js

--- tools/solution-checker.js
+++ tools/solution-checker.js
@@ -1,13 +1,14 @@
 const utils = require('./utils');
 
 (async () => {
-  if (process.argv.length !== 4) {
-    console.error("node checker.js task.desc solution.sol");
+  if (process.argv.length !== 4 && process.argv.length !== 5) {
+    console.error("node checker.js task.desc solution.sol [boosters.buy]");
     process.exit(-1);
   }
   const taskPath = process.argv[2];
   const solutionPath = process.argv[3];
-  const result = await utils.checkSolution(taskPath, solutionPath);
+  const boostersPath = process.argv.length === 5 ? process.argv[4] : null;
+  const result = await utils.checkSolution(taskPath, solutionPath, boostersPath);
   console.log(JSON.stringify(result));
 
   if (!result.success) {
--- tools/utils.js
+++ tools/utils.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-async function checkSolution(taskPath, solutionPath) {
+async function checkSolution(taskPath, solutionPath, boostersPath) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://icfpcontest2019.github.io/solution_checker/');
@@ -13,13 +13,20 @@ async function checkSolution(taskPath, solutionPath) {
   await solution.uploadFile(solutionPath);
   await page.waitForFunction('document.querySelector("#output").innerText === "Done uploading solution"');
 
+  if (boostersPath) {
+    const boosters = await page.$('#submit_boosters');
+    await boosters.uploadFile(boostersPath);
+    await page.waitForFunction('document.querySelector("#output").innerText === "Done uploading solution"');
+    // TODO: need to wait? (like checkPuzzle)
+  }
+
   await page.click('#execute_solution');
   await page.waitForFunction('/^(Success|Failed)/.test(document.querySelector("#output").innerText)');
   const output = await page.$eval("#output", output => output.textContent);
   await browser.close();
 
   const success = output.startsWith('Success');
-  const result = { success, timeunits: success ? parseInt(output.match(/[0-9]+/)[0], 10) : null };
+  const result = { success, timeunits: success ? parseInt(output.match(/[0-9]+/)[0], 10) : null, message: output };
   return result;
 }
 
